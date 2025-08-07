0f22cd59e7e40e8998a3ac94036f6950e3d89aaa
SpringWater42 <aashiqbenny2@gmail.com>
2025-08-07 21:17:49 +0200

    Displaying

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+
+const API_URL = 'http://localhost:9090';
 
 export default createStore({
   state: {
@@ -12,6 +15,15 @@ export default createStore({
     },
   },
   actions: {
+        async getProducts({ commit }, payload) {
+      try {
+        const response = await axios.get(`${API_URL}/products`, payload);
+        commit('setProducts', response.data.products);
+      } catch (error) {
+        // console.error("Error getting products:", error);
+      }
+    },
+
   },
   modules: {
   }
--- Frontend/src/views/AboutView.vue
+++ Frontend/src/views/AboutView.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="about">
-
+    {{ products }}
   </div>
 </template>
 
-
 <script>
 export default {
-   
-}
-
-
+  data() {
+    return {
+      products: {
+        products_id: null,
+        name: "",
+        description: "",
+        price: 0,
+        discount: 0,
+        stock: 0,
+        category_id: null,
+        image_url: "",
+        is_new: false,
+      },
+    };
+  },
+  computed: {
+    products() {
+      return this.$store.state.products || [];
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getProducts");
+  },
+};
 </script>
